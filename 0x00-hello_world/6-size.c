4fd44aa864beb9961cf1094f14d2e6050bf8c3ad
NessmaKandil <nessmakandil137@gmail.com.com>
2023-07-18 08:51:59 +0300

    C programming

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,4 +1,9 @@
 #include<stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 
 int main(void)
 { 
@@ -8,5 +13,5 @@ int main(void)
 	printf("Size of a long long int: %d byte(s)\n", sizeof(long long int));
 	printf("Size of a float: %d byte(s)\n", sizeof(float));
 
-	return(0);
+	return (0);
 }
