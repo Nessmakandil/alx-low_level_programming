bba5f2c26070c2837b9568442669823efe11bbd6
NessmaKandil <nessmakandil137@gmail.com.com>
2023-07-20 02:29:22 +0300

    C programming

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -12,14 +12,17 @@ void print_times_table(int n)
 {
         int i, j;
 
-        for (i = 0; i <= n; i++)
-        {
-		for (j = 0; j <= n; j++)
+	while (n >= 0 && n <= 15)
+	{
+		for (i = 0; i <= n; i++)
         	{
-			printf("%d", i*j);
-			if (j != n)
-				printf(", ");
-		}
-		printf("\n");
-         }
+			for (j = 0; j <= n; j++)
+        		{
+				printf("%d", i*j);
+				if (j != n)
+					printf(", ");
+			}
+			printf("\n");
+         	}
+	}
 }
