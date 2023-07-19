d42efb5c86678cf5fb38b885b3fe8c09374654f0
NessmaKandil <nessmakandil137@gmail.com.com>
2023-07-19 03:30:30 +0300

    C programming

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include"main.h"
+/**
+ *  print_alphabet - print lowercase alphabets from a to z - 10 times.
+ *
+ * Return: Always 0.
+ */
+
+void print_alphabet(void)
+{
+	int i;
+	
+        for (i = 0; i <= 10; i++)
+	{
+		char c;
+
+        	for (c = 'a'; c <= 'z'; ++c)
+        	{
+                	printf("%c", c);
+        	}
+        	printf("\n");
+	}
+}
