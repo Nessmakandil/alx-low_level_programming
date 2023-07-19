76ff028bbd75ca9c40a3c2fc7a31f530f5d2e415
NessmaKandil <nessmakandil137@gmail.com.com>
2023-07-20 02:58:25 +0300

    C programming

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -13,10 +13,26 @@ void times_table(void)
         {
 		for (j = 0; j <= 9; j++)
 		{
-			printf("%d", i*j);
-			if (j != 9)
-				printf(", ");
+			k = j * i;
+			if (j == 0)
+			{
+				_putchar(k + '0');
+			}
+
+			if (k < 10 && j != 0)
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar(' ');
+				_putchar(k + '0');
+			} else if (k >= 10)
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar((k / 10) + '0');
+				_putchar((k % 10) + '0');
+			}
 		}
-		printf("\n");
+		_putchar('\n');		
         }
 }
