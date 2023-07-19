b749aa452b0a08615f269dcf43f389d542858838
NessmaKandil <nessmakandil137@gmail.com.com>
2023-07-20 00:39:42 +0300

    C programming

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include"main.h"
+/**
+ * add -  adds two integers.
+ *
+ * @n1: is the first number to be added
+ *
+ * @n2: is the first number to be added
+ *
+ * Return: the value of the addition.
+ *
+ */
+
+int add(int n1, int n2)
+{
+	int result = n1 + n2;
+        return (result);        }
+}
