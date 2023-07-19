b3ce99b3dbb8fb22000a43549d220dd1d112e12b
NessmaKandil <nessmakandil137@gmail.com.com>
2023-07-19 15:31:09 +0300

    C programming

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -13,10 +13,10 @@ int print_last_digit(int n)
 {
 	if (n >= 0)
 	{
-		return (int(n%10));
+		return (n%10);
 	}
 	else
 	{
-                return (int((-1*n)%10));
+                return ((-1*n)%10);
 	}
 }
