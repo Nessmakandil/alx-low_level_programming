735d66af33bbdfe18c49ee69f420f3a41f888497
NessmaKandil <nessmakandil137@gmail.com.com>
2023-07-20 03:36:26 +0300

    C programming

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -5,19 +5,19 @@
  * Return: Always 0.
  */
 
-int main(void) 
+int main(void)
 {
 	int a = 1, b = 2, c, sum = 2;
 	/**
 	 * start with b, which is even
 	 */
-	
-	while (b <= 4000000) 
+
+	while (b <= 4000000)
 	{
 		c = a + b;
 		a = b;
 		b = c;
-		if (b % 2 == 0) 
+		if (b % 2 == 0)
 		{
 			sum += b;
 		}
