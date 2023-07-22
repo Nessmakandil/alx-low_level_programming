852add8606fa58d61edc3a0f368561f97cc163da
NessmaKandil <nessmakandil137@gmail.com.com>
2023-07-22 13:53:42 +0300

    C programming

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * _isupper - checks for uppercase character.
+ *
+ * @c: number to be checked.
+ *
+ * Return: 1 if c is a digit and 0 O.W.
+ */
+
+int _isdigit(int c)
+{
+        if (c < 48 || c > 57)
+        {
+                return (0);
+        }
+        else
+        {
+                return (1);
+        }
+}
