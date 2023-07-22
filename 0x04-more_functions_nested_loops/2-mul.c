7ceb2a46f404046795685a0c3b50da2cf373b355
NessmaKandil <nessmakandil137@gmail.com.com>
2023-07-22 14:26:21 +0300

    C programming

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _isupper - checks for uppercase character.
+ *
+ * @a: first number to be multiplied.
+ *
+ * @b: second number to be multiplied.
+ *
+ * Return: 1 if c is a digit and 0 O.W.
+ */
+
+int mul(int a, int b)
+{
+        return (a*b);
+}
