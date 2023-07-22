a030da9397eb4ff920e046ea1f7e34925edd9306
NessmaKandil <nessmakandil137@gmail.com.com>
2023-07-22 15:27:06 +0300

    C programming

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_most_numbers - prints the numbers, from 0 to 9 except 2, 4.
+ */
+
+void print_most_numbers(void)
+{
+	int i;
+
+	for (i = 48; i < 58; i++)
+	{
+		if (i != 52 && i != 50)
+			_putchar(i);
+	}
+	_putchar('\n');
+}
