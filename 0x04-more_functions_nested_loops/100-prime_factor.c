ebf38c944d25d4552c152f71142ce8452e3dcb19
NessmaKandil <nessmakandil137@gmail.com.com>
2023-07-23 23:43:08 +0300

    C programming

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -7,26 +7,27 @@
 
 int main(void)
 {
-	long int x, i, j, count, prime;
+        long int x, i, j, count, prime;
 
-	x = 612852475143;
+        x = 612852475143;
 
-	for (i = 1; i <= x; i++)
-	{
-		if (x % i == 0)
-		{
-			count = 0;
-			for (j = 1; j <= i; j++)
-			{
-				if (i % j == 0)
-				{
-					count++;
-				}
-			}
-			if (count == 2)
-				 prime = i; 
-		}
-	}
-	printf("%ld\n", prime);
-	return (0);	
+        for (i = 1; i <= x; i++)
+        {
+                if (x % i == 0)
+                {
+                        count = 0;
+                        for (j = 1; j <= i; j++)
+                        {
+                                if (i % j == 0)
+                                {
+                                        count++;
+                                }
+                        }
+                        if (count == 2)
+                                 prime = i;
+                }
+        }
+        printf("%ld\n", prime);
+        return (0);
 }
+ 
