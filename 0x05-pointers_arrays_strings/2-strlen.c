54b54dea41e26ed8a9d7dba9770b69ec642e8c73
NessmaKandil <nessmakandil137@gmail.com.com>
2023-07-25 02:57:05 +0300

    C programming

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * _strlen - computes the length of a string.
+ *
+ * @s: the input string.
+ *
+ * Return: returns the length of a string.
+ */
+
+int _strlen(char *s)
+{
+	int count;
+	
+	count = 0;
+	while (*s != '\0')
+	{
+		count++;
+		s++;
+	}
+	return (count);
+
+}
