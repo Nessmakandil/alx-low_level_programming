dc126f8b0e7f343ed4c3c37b952fb441d0b0dbdb
NessmaKandil <nessmakandil137@gmail.com.com>
2023-07-26 00:35:20 +0300

    C programming

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -3,22 +3,23 @@
 /**
  * rev_string - prints a string, in reverse.
  *
- * @str: string to be printed.
+ * @s: string to be printed.
  */
 
 void rev_string(char *s)
 {
-        int i, count;
-	char str = s[0];
+	int i, count;
+	char str;
 
-        count = 0;
+	str = s[0];
+	count = 0;
 
-        while (s[count] != '\0')
-        {
-                count++;
-        }
-        for (i = 0; i < count; i++)
-        {
+	while (s[count] != '\0')
+	{
+		count++;
+	}
+	for (i = 0; i < count; i++)
+	{
 		count--;
 		str = s[i];
 		s[i] = s[count];
