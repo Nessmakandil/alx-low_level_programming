cf27f3ac3e4cee5e99c9a20b923b4320d402da51
NessmaKandil <nessmakandil137@gmail.com.com>
2023-07-25 05:28:15 +0300

    C programming

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -11,16 +11,17 @@ void print_rev(char *str)
 	int i, count;
 
 	count = 0;
-        while (*str != '\0')
-        {
-                count++;
-                str++;
-        }
+
+	while (*str != '\0')
+	{
+		count++;
+		str++;
+	}
 	str--;
 	for (i = count-1; i >= 0; i--)
 	{
 		printf("%c", *str);
 		str--;
 	}
-        printf("\n");
+	printf("\n");
 }
