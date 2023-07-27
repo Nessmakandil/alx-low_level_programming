2a47852c8cfb06f4c65d18b7f57104b969ddb258
NessmaKandil <nessmakandil137@gmail.com.com>
2023-07-27 17:44:16 +0300

    C programming

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -9,29 +9,24 @@
 
 char *cap_string(char *str)
 {
-	int i, count;
+	int i;
 
-	count = 0;
-
-	while (str[count] != '\0')
-		count++;
-
-	for (i = 0; i < count; i++)
+	for (i = 0; str[i] != '\0'; i++)
 	{
-		if ((str[i] == ',' ||\
-		    str[i] == ';' ||\
-		    str[i] == '.' ||\
-		    str[i] == '!' ||\
-		    str[i] == '?' ||\
-		    str[i] == ')' ||\
-		    str[i] == '}' ||\
-		    str[i] == '\t' ||\
-		    str[i] == '\n' ||\
-		    str[i] == '"' ||\
-		    str[i] == ' ') &&
-		   (str[i + 1] >= 'a' && 
-		    str[i + 1] <= 'z'))
-			str[i + 1] = str[i + 1] - 32;
+		if ((str[i - 1] == ',' ||\
+		    str[i - 1] == ';' ||\
+		    str[i - 1] == '.' ||\
+		    str[i - 1] == '!' ||\
+		    str[i - 1] == '?' ||\
+		    str[i - 1] == ')' ||\
+		    str[i - 1] == '}' ||\
+		    str[i - 1] == '\t' ||\
+		    str[i - 1] == '\n' ||\
+		    str[i - 1] == '"' ||\
+		    str[i - 1] == ' ') &&
+		   (str[i] >= 'a' && 
+		    str[i] <= 'z'))
+			str[i] = str[i] - 32;
 
 	}
 	return (str);
