2f7b0677574d690faa3bc744a82a28d647e49d14
NessmaKandil <nessmakandil137@gmail.com.com>
2023-07-27 03:10:17 +0300

    C programming

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * _strncat - similar to the _strcat,
+ * it will use at most n bytes from src,
+ * src does not need to be null-terminated
+ * if it contains n or more bytes.
+ *
+ * @dest: Input string.
+ *
+ * @
