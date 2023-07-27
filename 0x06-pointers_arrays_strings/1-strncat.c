84469297ca01cccaf687fed352fac20ac1c05175
NessmaKandil <nessmakandil137@gmail.com.com>
2023-07-27 03:47:48 +0300

    C programming

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -7,4 +7,29 @@
  *
  * @dest: Input string.
  *
- * @
+ * @src: Input string.
+ *
+ * @n: Input Integer.
+ *
+ * Return: A pointer to the resulting string dest.
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int count1, count2, i;
+
+	count1 = 0;
+	count2 = 0;
+
+	while (dest[count1] != '\0')
+		count1++;
+	while (src[count2] != '\0')
+		count2++;
+	if (n > count2)
+		n = count2;
+	for (i = 0; i < n; i++)
+	{
+		dest[count1 + i] = src[i];
+	}
+	return (dest);
+}
