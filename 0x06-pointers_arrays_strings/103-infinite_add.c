8bd83f8580ddf3a17156aff717b1e0540ea45d54
NessmaKandil <nessmakandil137@gmail.com.com>
2023-07-29 04:59:44 +0300

    C programming

---
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * infinite_add - adds two numbers.
+ *
+ * @n1: input number
+ *
+ * @n2: input number
+ *
+ * @r: the buffer that the function will use to store the result
+ *
+ * @size_r: buffer size.
+ *
+ * Return: A pointer to char.
+ */
+
+char *infinite_add(char *n1, char *n2, char *r, int size_r)
+{
+}
---
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * print_buffer - prints a buffer.
+ *
+ * @b: input buffer.
+ *
+ * @size: buffer length.
+ *
+ */
+
+void print_buffer(char *b, int size)
+{
+}
