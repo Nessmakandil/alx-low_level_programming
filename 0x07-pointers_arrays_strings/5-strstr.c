6f8846027283622df363b89cd4381f3c9aa6d270
NessmaKandil <nessmakandil137@gmail.com.com>
2023-08-02 21:44:48 +0300

    C programming

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -13,25 +13,24 @@
 
 char *_strstr(char *haystack, char *needle)
 {
-    if (*needle == '\0') {
-        return haystack;
-    }
+	if (*needle == '\0')
+	{
+        	return haystack;
+    	}
+	while (*haystack != '\0')
+	{
+		char *h = haystack;
+		char *n = needle;
 
-    while (*haystack != '\0') {
-        char *h = haystack;
-        char *n = needle;
-
-        while (*n != '\0' && *h == *n) {
-            h++;
-            n++;
-        }
-
-        if (*n == '\0') {
-            return haystack;
-        }
-
-        haystack++;
-    }
-
-    return NULL;
+		while (*n != '\0' && *h == *n)
+		{
+			h++;
+			n++;
+		}
+		if (*n == '\0') {
+			return haystack;
+		}
+		haystack++;
+	}
+	return NULL;
 }
