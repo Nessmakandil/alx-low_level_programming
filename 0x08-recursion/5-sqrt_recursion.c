67ca55f144a005892539dcc6f7d78b475c7fdd65
NessmaKandil <nessmakandil137@gmail.com.com>
2023-08-04 03:32:17 +0300

    C programming

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,4 +1,5 @@
 #include "main.h"
+
 /** _sqrt_recursion - a function that returns the natural square root
  * of a number.
  *
@@ -12,10 +13,10 @@ int _sqrt_recursion(int n)
 {
 	int ones = n - (n / 10) * 10;
 
-	if (n == 1 || n == 0) 
+	if (n == 1 || n == 0)
 		return (n);
 	if (n < 0 || ones == 2 || ones == 3 || ones == 7 || ones == 8)
-		return (-1);	
+		return (-1);
 	return (helper(n, 0));
 }
 
@@ -29,10 +30,10 @@ int _sqrt_recursion(int n)
 
 int helper(int n, int i)
 {
-        if (i * i == n)
+	if (i * i == n)
                 return (i);
-        if (i * i > n)
-                return (-1);
-        return (helper(n, i + 1));
+	if (i * i > n)
+		return (-1);
+	return (helper(n, i + 1));
 }
 
