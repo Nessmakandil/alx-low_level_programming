2df5781cd270d07e454820f68efe25523201299b
NessmaKandil <nessmakandil137@gmail.com.com>
2023-08-08 13:56:41 +0300

    C programming

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -8,11 +8,8 @@
  * Return: 0.
  */
 
-int main(int argc, char *argv[])
+int main(int argc, char *argv[] __attribute__((unused)))
 {
-	printf("%d\n", argc - 1);
-	
-	argv++;
-	
-	return (0);
+		printf("%d\n", argc - 1);
+		return (0);
 }
