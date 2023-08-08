747fa67877feaf0892c8e5245d91bfc18531e418
NessmaKandil <nessmakandil137@gmail.com.com>
2023-08-08 14:54:22 +0300

    C programming

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -10,13 +10,14 @@
 
 int main(int argc, char *argv[])
 {
-	int mul = 1;
+	int mul;
 
 	if (argc < 3)
 	{
 		printf("Error\n");
 		return (1);
 	}
+
 	mul = atoi(argv[1]) * atoi(argv[2]);
 	
 	printf("%d\n", mul);
