209616de5c7b3b23d6ebbeb96c31eb30167ab7bb
NessmaKandil <nessmakandil137@gmail.com.com>
2023-08-08 14:02:30 +0300

    C programming

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -11,34 +11,14 @@
 
 int main(int argc, char *argv[])
 {
-        char *programName = argv[0];
-        char *baseName = NULL;
-        char *extension = NULL;
-		int i = 1;
+	int i = 1;
 
-        baseName = strrchr(programName, '\\');
+	printf("./%s\n", *argv);
 
-        if (baseName == NULL)
-        {
-                baseName = programName;
-        }
-        else
-        {
-                baseName++;
-        }
-
-        extension = strrchr(baseName, '.');
-
-        if (extension != NULL)
-        {
-                *extension = '\0';
-        }
-        printf("./%s\n", baseName);
-
-		while (i < argc)
-		{
-			printf("%s\n", argv[i]);
-			i++;
-		}
-		return (argc);
+	while (i < argc)
+	{
+		printf("%s\n", argv[i]);
+		i++;
+	}
+	return (0);
 }
