ff43f697ba52c78f85af0cef331391f5b899e9e2
NessmaKandil <nessmakandil137@gmail.com.com>
2023-08-08 07:15:15 +0300

    C programming

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,5 +1,13 @@
 #include <stdio.h>
 #include <string.h>
+/**
+ * main - a program that prints its name, followed by a new line.
+ *
+ * @argc: count of argv.
+ * @argv: string.
+ *
+ * Return: integer count of argc.
+ */
 
 int main(int argc, char *argv[])
 {
