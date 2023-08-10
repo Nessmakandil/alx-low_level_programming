e3353a6c51e36adf605dc897891755d7b607cdfb
NessmaKandil <nessmakandil137@gmail.com.com>
2023-08-10 06:54:08 +0300

    C programming

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -25,7 +25,7 @@ int main(int argc, char *argv[])
 	{
 		c = argv[i];
 		len = strlen(c);
-		for (; j < len; j++)
+		for (j = 0; j < len; j++)
 		{
 			if (!isdigit(c[j]))
 			{
@@ -33,8 +33,7 @@ int main(int argc, char *argv[])
 				return (1);
 			}
 		}
-		add = add + atoi(argv[i]);
-		
+		add = add + atoi(argv[i]);		
 	}
 	printf("%d\n", add);
 	return (0);
