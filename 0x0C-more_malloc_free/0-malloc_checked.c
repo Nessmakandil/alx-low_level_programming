322c3d506c614d741ea8eb0564e3c3990efd5805
NessmaKandil <nessmakandil137@gmail.com.com>
2023-08-10 22:43:57 +0300

    C programming

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -1,18 +1,19 @@
 #include "main.h"
+
 /**
- * malloc_checked - a function that allocates memory using malloc.
- *
- * @b: input size.
+ * *malloc_checked - allocates memory using malloc
+ * @b: number of bytes to allocate
  *
  * Return: a pointer to the allocated memory
  */
-
 void *malloc_checked(unsigned int b)
 {
-	void *p = malloc(b);
+	void *ptr;
 
-	if (p == NULL)
-		exit (98);
-		
-	return (p);
+	ptr = malloc(b);
+
+	if (ptr == NULL)
+		exit(98);
+
+	return (ptr);
 }
--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,1 +1,16 @@
 #include "main.h"
+/**
+ * string_nconcat - a function that concatenates two strings.
+ *
+ * @s1: input string.
+ * @s2: input string.
+ * @n: input size.
+ * Return: a pointer to the allocated memory
+ */
+
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	if ((void*)malloc(b) == NULL)
+		exit (98);
+	return ((void*)malloc(b));
+}
