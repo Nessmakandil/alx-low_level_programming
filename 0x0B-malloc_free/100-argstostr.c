34d13ce3bc493851e9a7930936cba5ab1df08559
NessmaKandil <nessmakandil137@gmail.com.com>
2023-08-13 22:58:06 +0300

    C programming

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -1,36 +1,44 @@
 #include "main.h"
 #include <string.h>
+/**
+ * argstostr - a function that concatenates all the arguments of your program.
+ *
+ * @ac: the number of arguments.
+ * @av: the arguments of the program.
+ * 
+ * Return: concatinated string.
+ */
+
 char *argstostr(int ac, char **av)
 {
-    int i, x;
-    unsigned int j;
-    unsigned int sum = 0;
-    char *ptr;
+	int i, x;
+	unsigned int j;
+	unsigned int sum = 0;
+	char *ptr;
 
-    if (ac == 0 || av == NULL)
-    {
-        return (NULL);
-    }
+	if (ac == 0 || av == NULL)
+	{
+		return (NULL);
+	}
+	for (i = 0; i < ac; i++)
+	{
+		sum = sum + strlen(av[i]) + 1;
+	}
 
-    for (i = 0; i < ac; i++)
-    {
-        sum = sum + strlen(av[i]) + 1;
-    }
+	ptr = malloc(sum + 1);
+	x = 0;
 
-    ptr = malloc(sum + 1);
-    x = 0;
-
-    for (i = 0; i < ac; i++)
-    {
-        j = 0;
-        while (j < strlen(av[i]))
-        {
-            ptr[x] = av[i][j];
-            j++;
-            x++;
-        }
-        ptr[x] = '\n';
-        x++;
-    }
-    return (ptr);
+	for (i = 0; i < ac; i++)
+	{
+		j = 0;
+		while (j < strlen(av[i]))
+		{
+			ptr[x] = av[i][j];
+			j++;
+			x++;
+		}
+		ptr[x] = '\n';
+		x++;
+	}
+	return (ptr);
 }
