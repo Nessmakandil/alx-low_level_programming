03713f4039ba76fee280428a9a62f0d4847ef289
NessmaKandil <nessmakandil137@gmail.com.com>
2023-08-14 13:48:42 +0300

    C programming

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -43,7 +43,7 @@ char **strtow(char *str)
             x++;
             count = 0;
         }
-        else if (str[i] != '\t' && str[i] != '\0' && str[i] != ' ')
+        else if (str[i] != '\t' && str[i] != ' ')
         {
             count++;            
         }
