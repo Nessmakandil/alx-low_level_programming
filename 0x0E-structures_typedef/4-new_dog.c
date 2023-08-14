9a5d7ae77aaedf6cf79918435ecaa2a2fb26b945
NessmaKandil <nessmakandil137@gmail.com.com>
2023-08-15 00:30:46 +0300

    C programming

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,28 @@
+#include "dog.h"
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *new;
+	int i, j;
+	int count1 = 0;
+	int count2 = 0;
+
+	new = malloc(sizeof(dog_t));
+	
+	if (new == NULL)
+		return (NULL);
+
+	while (name[count1])
+		count1++;
+	while (owner[count2])
+		count2++;
+
+	new->name = malloc(count1 + 1);
+	new->owner = malloc(count2 + 1);
+
+	for (i = 0; i <= count1; i++)
+		new->name[i] = name[i];
+	for (j = 0; j <= count2; j++)
+		new->owner[j] = owner[j];
+	new->age = age;
+	return (new);
+}
