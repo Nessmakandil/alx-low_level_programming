197d4e2621928f84bb2a30dcdf888a50e216a7a5
Nessmakandil <nessmakandil137@gmail.com>
2023-08-21 10:17:36 +0300

    C programming

---
+++ printf/_printf.c
@@ -0,0 +1,48 @@
+#include "main.h"
+
+int _printf(const char *format, ...)
+{
+	    va_list vl;
+	    int i = 0, j=0;
+		char buff[100]={0}, tmp[20];
+		va_start(vl, format); 
+		while (format && format[i])
+		{
+		  	if(format[i] == '%')
+		  	{
+ 		    i++;
+ 		    switch (format[i]) 
+ 		    {
+	 		    case 'c': 
+	 		    {
+	 		        buff[j] = (char)va_arg( vl, int );
+	 		        j++;
+	 		        break;
+	 		    }
+	 		    case 'd': 
+	 		    {
+	 		        itoa(va_arg( vl, int ), tmp, 10);
+	 		        strcpy(&buff[j], tmp);
+	 		        j += strlen(tmp);
+		           break;
+		        }
+		        case 'x': 
+		        {
+		           itoa(va_arg( vl, int ), tmp, 16);
+		           strcpy(&buff[j], tmp);
+		           j += strlen(tmp);
+		           break;
+		        }
+        	}
+     	} 
+     	else 
+	    {
+	       	buff[j] = format[i];
+	       	j++;
+	    }
+	    i++;
+	} 
+    fwrite(buff, j, 1, stdout); 
+    va_end(vl);
+    return j;
+}
---
+++ printf/main.h
@@ -0,0 +1,8 @@
+
+#include <limits.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
+#include <string.h>
+
+int _printf(const char *format, ...);
