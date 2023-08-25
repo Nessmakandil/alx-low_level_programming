115d6c9187adffde8258e3c1bd8035c41c24fc05
Nessmakandil <nessmakandil137@gmail.com>
2023-08-25 23:49:09 +0300

    C programming

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * printStatement - print.
+ * 
+ * @printStatementInitialized: global value.
+ * 
+ */
+
+int printStatementInitialized = 0;
+
+void __attribute__((constructor)) printStatement()
+{
+    if (!printStatementInitialized)
+    {
+        printf("You're beat! and yet, you must allow,\n");
+        printf("I bore my house upon my back!\n");
+        printStatementInitialized = 1;
+    }
+}
