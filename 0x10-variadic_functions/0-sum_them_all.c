058238862cb29c181774fe9d01a62600162b83f9
Nessmakandil <nessmakandil137@gmail.com>
2023-08-26 09:29:05 +0300

    C programming

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,22 @@
+#include "variadic_functions.h"
+
+/**
+ * sum_them_all - sum all inputs.
+ *
+ * Return: Always 0.
+ */
+int sum_them_all(const unsigned int n, ...)
+{
+    va_list list;
+    unsigned int i;
+    int sum = 0;
+    
+    va_start(list, n);
+    
+    for (i = 0; i < n; i++)
+    {
+        sum = sum + va_arg(list, int);
+    }
+    va_end(list);
+    return (sum);
+}
