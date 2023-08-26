029409d36c89d1918394442f0529112f4d996b3d
Nessmakandil <nessmakandil137@gmail.com>
2023-08-26 12:36:41 +0300

    C programming

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -12,43 +12,47 @@ void print_all(const char * const format, ...) {
     int intValue;
     float floatValue;
     char* stringValue;
+    int isFirstItem = 1; 
 
     va_list args;
     va_start(args, format);
 
     ptr = format;
 
-    while (*ptr)
-    {
-        if (*ptr == 'c')
-        {
+    while (*ptr) {
+        if (*ptr == 'c') {
             ch = va_arg(args, int);
-            printf("%c", ch);
-        }
-        else if (*ptr == 'i')
-        {
-            intValue = va_arg(args, int);
-            printf("%d", intValue);
-        }
-        else if (*ptr == 'f')
-        {
-            floatValue = va_arg(args, double);
-            printf("%f", floatValue);
-        }
-        else if (*ptr == 's')
-        {
-            stringValue = va_arg(args, char*);
-            if (stringValue == NULL)
-            {
-                printf("(nil)");
+            if (!isFirstItem) {
+                printf(", ");
             }
-            else
-            {
+            printf("%c", ch);
+        } else if (*ptr == 'i') {
+            intValue = va_arg(args, int);
+            if (!isFirstItem) {
+                printf(", ");
+            }
+            printf("%d", intValue);
+        } else if (*ptr == 'f') {
+            floatValue = va_arg(args, double);
+            if (!isFirstItem) {
+                printf(", ");
+            }
+            printf("%f", floatValue);
+        } else if (*ptr == 's') {
+            stringValue = va_arg(args, char*);
+            if (!isFirstItem) {
+                printf(", ");
+            }
+            if (stringValue == NULL) {
+                printf("(nil)");
+            } else {
                 printf("%s", stringValue);
             }
         }
+        isFirstItem = 0;
         ptr++;
     }
+
     va_end(args);
     printf("\n");
 }
