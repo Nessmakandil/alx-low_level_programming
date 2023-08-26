542d879492ddef3a614b14e6313ee72bb2450549
Nessmakandil <nessmakandil137@gmail.com>
2023-08-26 19:42:56 +0300

    C programming

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,14 +1,11 @@
 global    main
-extern    printf
+          extern    printf
+		  format: db "Hello, Holberton\n",0
 
-section .data
-    format db "Hello, Holberton\n", 0
-
-section .text
 main:
-    mov   edi, format      ; set format string
-    xor   eax, eax         ; clear eax
-    call  printf           ; call printf
+	  mov   edi, format
+	  xor   eax, eax
+	  call  printf
+	  mov 	eax, 0
+	  ret
 
-    mov   eax, 0           ; return 0
-    ret                    ; exit the program
