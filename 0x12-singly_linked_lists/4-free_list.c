6c16e61f7e1586bf1868373007dec06ad12899a8
Nessmakandil <nessmakandil137@gmail.com>
2023-08-26 15:14:50 +0300

    C programming

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -9,14 +9,15 @@
 
 void free_list(list_t *head)
 {
-    list_t *current, *temp;
+	list_t *current, *temp;
 
-    current = head;
-    while (current != NULL)
-    {
-        temp = current;
-        current = current->next;
-        free(temp->str);
-        free(temp);
-    }
+	current = head;
+
+	while (current != NULL)
+	{
+		temp = current;
+		current = current->next;
+		free(temp->str);
+		free(temp);
+	}
 }
