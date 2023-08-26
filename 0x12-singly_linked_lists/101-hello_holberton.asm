31493d2c04785126e989d04db12e6c5de3904d24
Nessmakandil <nessmakandil137@gmail.com>
2023-08-26 17:48:17 +0300

    C programming

---
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -0,0 +1,20 @@
+section .data
+    hello db "Hello, Holberton", 0
+    format db "%s", 0
+
+section .text
+    extern printf
+
+global main
+main:
+    sub rsp, 8                ; align stack pointer
+
+    mov rdi, format           ; set format string
+    mov rsi, hello            ; set hello string
+    xor eax, eax              ; clear eax
+    call printf               ; call printf
+
+    add rsp, 8                ; restore stack pointer
+    xor eax, eax              ; clear eax
+
+    ret                       ; exit the program
