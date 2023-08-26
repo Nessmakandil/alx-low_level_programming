32b67ea1aff0192acb6427bbe811a6f29a4e9bd8
Nessmakandil <nessmakandil137@gmail.com>
2023-08-26 15:55:12 +0300

    C programming

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -4,9 +4,8 @@
  * printStatement - print.
  */
 
-
 void __attribute__((constructor)) printStatement()
 {
-    printf("You're beat! and yet, you must allow,\n");
-    printf("I bore my house upon my back!\n");
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
 }
