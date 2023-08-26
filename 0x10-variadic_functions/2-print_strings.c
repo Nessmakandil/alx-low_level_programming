7faf188a704fabb6e7b185be9254d524f240570a
Nessmakandil <nessmakandil137@gmail.com>
2023-08-26 11:01:31 +0300

    C programming

---
+++ 0x10-variadic_functions/2-print_strings.c
@@ -0,0 +1,27 @@
+#include "variadic_functions.h"
+/**
+ * print_strings - print.
+ *
+ * @separator: string to be printed between numbers.
+ * @n: number of inputs.
+ *
+ */
+
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	va_list list;
+	unsigned int i;
+
+	va_start(list, n);
+
+	for (i = 0; i < n; i++)
+	{
+		printf("%s", va_arg(list, char *));
+        if (separator != NULL && i != n - 1)
+        {
+            printf("%s", separator);
+        }     
+	}
+    printf("\n");
+	va_end(list);
+}
