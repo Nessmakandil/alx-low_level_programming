6406c1902835d7d5ba53ee3cb2b7b1d3acc8aced
Nessmakandil <nessmakandil137@gmail.com>
2023-08-26 10:58:32 +0300

    C programming

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -20,7 +20,6 @@ void print_numbers(const char *separator, const unsigned int n, ...)
         if (separator != NULL && i != n - 1)
         {
             printf("%s", separator);
-            printf(" ");
         }     
 	}
     printf("\n");
