e468359e373d9a3556f73f41a5abd14b41f899bd
Nessmakandil <nessmakandil137@gmail.com>
2023-08-26 12:40:56 +0300

    C programming

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -12,47 +12,43 @@ void print_all(const char * const format, ...) {
     int intValue;
     float floatValue;
     char* stringValue;
-    int isFirstItem = 1; 
 
     va_list args;
     va_start(args, format);
 
     ptr = format;
 
-    while (*ptr) {
-        if (*ptr == 'c') {
+    while (*ptr)
+    {
+        if (*ptr == 'c')
+        {
             ch = va_arg(args, int);
-            if (!isFirstItem) {
-                printf(", ");
-            }
             printf("%c", ch);
-        } else if (*ptr == 'i') {
+        }
+        else if (*ptr == 'i')
+        {
             intValue = va_arg(args, int);
-            if (!isFirstItem) {
-                printf(", ");
-            }
             printf("%d", intValue);
-        } else if (*ptr == 'f') {
+        }
+        else if (*ptr == 'f')
+        {
             floatValue = va_arg(args, double);
-            if (!isFirstItem) {
-                printf(", ");
-            }
             printf("%f", floatValue);
-        } else if (*ptr == 's') {
+        }
+        else if (*ptr == 's')
+        {
             stringValue = va_arg(args, char*);
-            if (!isFirstItem) {
-                printf(", ");
-            }
-            if (stringValue == NULL) {
+            if (stringValue == NULL)
+            {
                 printf("(nil)");
-            } else {
+            }
+            else
+            {
                 printf("%s", stringValue);
             }
         }
-        isFirstItem = 0;
         ptr++;
     }
-
     va_end(args);
     printf("\n");
 }
