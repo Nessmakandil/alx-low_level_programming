d27f86da4d9009352300361e7663377c744f9b09
Nessmakandil <nessmakandil137@gmail.com>
2023-08-27 00:01:53 +0300

    C programming

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -1,6 +1,6 @@
 #include "function_pointers.h"
 /**
- * print_name - a function that prints a name.
+ * array_iterator - a function that prints a name.
  *
  * @array: name.
  * @size: number.
@@ -10,14 +10,14 @@
 
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
-    size_t i;
+	size_t i;
 
-	if (action == NULL)
+	if (action == NULL || array == NULL)
 	{
 		return;
 	}
-    for (i = 0; i < size; i++)
-    {
-        (*action)(array[i]);
-    }	
+	for (i = 0; i < size; i++)
+	{
+		(*action)(array[i]);
+	}
 }
