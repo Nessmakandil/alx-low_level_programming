b338802fbbbd6cf30e7975f6105a25ed7b005551
Nessmakandil <nessmakandil137@gmail.com>
2023-08-27 06:42:00 +0300

    C programming

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -4,30 +4,30 @@
  * calculateMinCoins - calculate change.
  *
  * @cents: money.
- * 
+ *
  * Return: count;
  */
 int calculateMinCoins(int cents)
 {
-    int coins[] = {25, 10, 5, 2, 1};
-    int numCoins = sizeof(coins) / sizeof(coins[0]);
-    int count = 0;
-    int i;
-
-    if (cents < 0)
-    {
-        printf("0\n");
-        return 0;
-    }
-    for (i = 0; i < numCoins; i++)
-    {
-        while (cents >= coins[i])
-        {
-            cents -= coins[i];
-            count++;
-        }
-    }
-    return count;
+	int coins[] = {25, 10, 5, 2, 1};
+	int numCoins = sizeof(coins) / sizeof(coins[0]);
+	int count = 0;
+	int i;
+	
+	if (cents < 0)
+	{
+		printf("0\n");
+		return 0;
+	}
+	for (i = 0; i < numCoins; i++)
+	{
+		while (cents >= coins[i])
+		{
+			cents -= coins[i];
+			count++;
+		}
+	}
+	return (count);
 }
 /**
  * main - check.
@@ -39,22 +39,22 @@ int calculateMinCoins(int cents)
  */
 int main(int argc, char *argv[])
 {
-    int cents, minCoins;
+	int cents, minCoins;
 
-    if (argc != 2)
-    {
-        printf("Error\n");
-        return 1;
-    }
-    if (atoi(argv[1]) < 0)
-    {
-        printf("0\n");
-    }
-    else
-    {
-        cents = atoi(argv[1]);
-        minCoins = calculateMinCoins(cents);
-        printf("%d\n", minCoins);
-    }
-    return 0;
+	if (argc != 2)
+	{
+		printf("Error\n");
+		return 1;
+	}
+	if (atoi(argv[1]) < 0)
+	{
+		printf("0\n");
+	}
+	else
+	{
+		cents = atoi(argv[1]);
+		minCoins = calculateMinCoins(cents);
+		printf("%d\n", minCoins);
+	}
+	return (0);
 }
