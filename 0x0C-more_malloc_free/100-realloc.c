f1c76d12cf7e7b7ac7b9405dd496322d424e4ca6
Nessmakandil <nessmakandil137@gmail.com>
2023-08-27 23:44:30 +0300

    C programming

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -9,30 +9,32 @@
  *
  * Return: a pointer to the new allocated memory
  */
+
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
-    void *new_ptr;
+	void *new_ptr;
 
-    if (old_size == new_size)
-    {
-        return (ptr);
-    }
-    if (ptr == NULL)
-    {
-        ptr = malloc(new_size);
-        return (ptr);
-    }
-    if (ptr != NULL && new_size == 0)
-    {
-        free(ptr);
-        return (NULL);
-    }
-        
-    new_ptr = malloc(new_size);
-    if (new_ptr != NULL)
-    {
-        memcpy(new_ptr, ptr, (old_size < new_size) ? old_size : new_size);
-        free(ptr);
-    }
-     return (new_ptr);
+	if (old_size == new_size)
+	{
+		return (ptr);
+	}
+	if (ptr == NULL)
+	{
+		ptr = malloc(new_size);
+		return (ptr);
+	}
+	if (ptr != NULL && new_size == 0)
+	{
+		free(ptr);
+		return (NULL);
+	}
+
+	new_ptr = malloc(new_size);
+	
+	if (new_ptr != NULL)
+	{
+		memcpy(new_ptr, ptr, (old_size < new_size) ? old_size : new_size);
+		free(ptr);
+	}
+	return (new_ptr);
 }
