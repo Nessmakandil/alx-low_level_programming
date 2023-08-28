89c28922ed23980d10bc6b9cc3f577e56d237780
Nessmakandil <nessmakandil137@gmail.com>
2023-08-28 23:38:22 +0300

    C programming

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+/**
+ * free_listint2 - prints all the elements of a list_t list.
+ * @head: linked list.
+ * Return: the number of nodes.
+ */
+
+void free_listint2(listint_t **head)
+{
+	listint_t *current, *temp;
+
+	current = *head;
+
+	while (current != NULL)
+	{
+		temp = current;
+		current = current->next;
+		free(temp);
+	}
+}
