3a4e073415cdb984a98247f3c736a66bcb1925ba
Nessmakandil <nessmakandil137@gmail.com>
2023-08-28 22:53:41 +0300

    C programming

---
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -0,0 +1,16 @@
+#include "lists.h"
+/**
+ * add_nodeint - a function that prints all the elements of a listint_t list.
+ * @head: input list.
+ * @n: new node value .
+ * Return: number of elements.
+ */
+listint_t *add_nodeint(listint_t **head, const int n)
+{
+	listint_t *h = (listint_t *) malloc(sizeof(listint_t));
+
+    h->n = n;
+    h->next = *head;
+    *head = h;
+	return (h);
+}
