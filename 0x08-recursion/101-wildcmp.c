0d5dc1178fde0d84a71b46adea4fa17e9421e582
Nessmakandil <nessmakandil137@gmail.com>
2023-08-28 19:14:20 +0300

    C programming

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -8,16 +8,19 @@
  */
 int helper(char *s1, char *s2)
 {
-    if (*s1 == '\0' && *s2 == '\0') {
+    if (*s1 == '\0' && *s2 == '\0')
+    {
         return (1);
     }
-    if (*s2 == '*') {
+    if (*s2 == '*')
+    {
         if (*s1 == '\0')
             return (helper(s1, s2 + 1));
         else
             return (helper(s1 + 1, s2) || helper(s1, s2 + 1));
     }
-    if (*s1 == *s2) {
+    if (*s1 == *s2)
+    {
         return (helper(s1 + 1, s2 + 1));
     }
     return (0);
@@ -29,7 +32,8 @@ int helper(char *s1, char *s2)
  * @s2: input string.
  * Return: 1 for true, 0 for false.
  */
-int wildcmp(char *s1, char *s2) {
+int wildcmp(char *s1, char *s2)
+{
     return (helper(s1, s2));
 }
 
