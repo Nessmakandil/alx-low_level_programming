b1f1a95a0d12784f6a6a669ccb728184746d4d41
Nessmakandil <nessmakandil137@gmail.com>
2023-08-28 23:54:01 +0300

    C programming

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -7,15 +7,15 @@
 
 void free_listint2(listint_t **head)
 {
-	listint_t *current, *temp;
+	listint_t *current, *next_node;
 
 	current = *head;
 
 	while (current != NULL)
 	{
-		temp = current;
-		current = current->next;
-		free(temp);
+		next_node = current->next;  
+		free(current);
+		current = next_node;
 	}
-    *head = NULL;
+	*head = NULL;
 }
