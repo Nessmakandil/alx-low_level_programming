48b4107fed92499a513c96b27e7172bc2fa8f012
Nessmakandil <nessmakandil137@gmail.com>
2023-08-28 03:15:52 +0300

    C programming

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -74,6 +74,7 @@ char **strtow(char *str)
 	int wordscount = splir_words(str);
 	
 	ptr = (char **) malloc((wordscount + 1) * sizeof(char *));
+	memset(ptr, 0, (wordscount + 1));
 
 	if (wordscount == 0 || str == NULL || ptr == NULL)
 	{
@@ -85,7 +86,8 @@ char **strtow(char *str)
 	{
 		if ((str[i] == '\t' || str[i] == '\0' || str[i] == ' ') && count > 0)
 		{
-			ptr[x] = (char *) malloc((count + 1) * sizeof(char *));	
+			ptr[x] = (char *) malloc((count + 1) * sizeof(char *));
+			memset(ptr[x], 0, (count + 1));	
 			for (j = 0; j < count; j++)
 			{
 				ptr[x][j] = str[i - count + j];
