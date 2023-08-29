6805294c43f9b90ec391a65bcc6513f6a9e37859
Nessmakandil <nessmakandil137@gmail.com>
2023-08-29 23:20:16 +0300

    C programming

---
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+/**
+ * get_nodeint_at_index - prints all the elements of a list_t list.
+ * @head: linked list.
+ * @index: linked list.
+ * Return: the number of nodes.
+ */
+
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+{
+	listint_t *current;
+    unsigned int i = 0;
+
+	current = head;
+
+	while (i < index)
+	{
+		current = current->next;
+		i++;
+	}
+    return (current);
+}
