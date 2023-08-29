913c1b6221eb664c8d186f563bf593bce151d5f0
Nessmakandil <nessmakandil137@gmail.com>
2023-08-29 23:22:02 +0300

    C programming

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -13,6 +13,10 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 
 	current = head;
 
+	if (current == NULL)
+	{
+		return (current);
+	}
 	while (i < index)
 	{
 		current = current->next;
