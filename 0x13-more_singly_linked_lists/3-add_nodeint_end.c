129c4e575631d41d41a6d2b1aa6653e1850543b4
Nessmakandil <nessmakandil137@gmail.com>
2023-08-30 17:40:49 +0300

    C programming

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -1,6 +1,7 @@
 #include "lists.h"
 /**
- * add_nodeint_end - a function that prints all the elements of a listint_t list.
+ * add_nodeint_end - a function that prints all the element
+ * of a listint_t list.
  * @head: input list.
  * @n: new node value .
  * Return: new list.
@@ -8,14 +9,15 @@
 listint_t *add_nodeint_end(listint_t **head, const int n)
 {
 	listint_t *h = (listint_t *) malloc(sizeof(listint_t));
-    listint_t *new = (listint_t *) malloc(sizeof(listint_t));
-    
-    if (h == NULL || new == NULL)
+	listint_t *new = (listint_t *) malloc(sizeof(listint_t));
+
+	if (h == NULL || new == NULL)
 	{
 		free(h);
 		free(new);
 		return (NULL);
 	}
+	
 	new->n = n;
 	new->next = NULL;
 
@@ -32,5 +34,5 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 		}
 		h->next = new;
 	}
-    return (h);
+	return (h);
 }
