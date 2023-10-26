59b68710ab2b940a81ae1bacd29c81310221cea3
Nessmakandil <nessmakandil137@gmail.com>
2023-10-27 00:33:04 +0200

    c bit manipulation

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -9,17 +9,17 @@
 int set_bit(unsigned long int *n, unsigned int index)
 {
 	unsigned long int mask;
-	
+
 	if (index >= sizeof(unsigned long int) * 8)
 	{
-        return (-1);
-    }
+		return (-1);
+	}
 	mask = 1UL << index;
 	*n = *n | mask;
 	if (*n & mask)
 	{
 		return (1);
 	}
-	else 
+	else
 		return (-1);
 }
