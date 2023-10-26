fa28030e36bb20b6e02c932234e8422b0670340d
Nessmakandil <nessmakandil137@gmail.com>
2023-10-26 20:57:50 +0300

    c bit manipulation

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * get_endianness - checks the endianness
+ * Return: 0 if big endian, 1 if little endian
+ */
+int get_endianness(void)
+{
+	union {
+        int i;
+        char c[sizeof(int)];
+    } u;
+
+	u.i = 1;
+	return u.c[0] == 1 ? 1 : 0;
+	
+}
