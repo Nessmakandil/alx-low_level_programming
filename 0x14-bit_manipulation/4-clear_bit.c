6b1bbc3472d5205d52884fda30ad9dfb3d454e4a
Nessmakandil <nessmakandil137@gmail.com>
2023-10-27 00:26:11 +0200

    c bit manipulation

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -8,17 +8,17 @@
 int clear_bit(unsigned long int *n, unsigned int index)
 {
 	unsigned long int mask;
-	
+
 	if (index >= sizeof(unsigned long int) * 8)
 	{
-        return (-1);
-    }
+		return (-1);
+	}
 	mask = 1UL << index;
 	*n = *n & ~mask;
 	if (!(*n & mask))
 	{
 		return (1);
 	}
-	else 
+	else
 		return (-1);
 }
