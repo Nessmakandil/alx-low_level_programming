35deed94e9fd69dedf18e4f675fd535fd73be0c5
NessmaKandil <nessmakandil137@gmail.com>
2023-12-08 04:19:58 +0200

    task 6

---
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+/**
+ * sum_dlistint - adds all nodes
+ * @head: pointer to the first element of the list
+ * Return: sum
+ */
+int sum_dlistint(dlistint_t *head)
+{
+	int sum = 0;
+	dlistint_t *current = head;
+
+	while (current != NULL)
+	{
+		sum += current->n;
+		current = current->next;
+	}
+	return (sum);
+}
