89b8e12437e739065a6c424263ff061ce3f9bf1a
NessmaKandil <nessmakandil137@gmail.com>
2023-12-12 00:54:54 +0200

    Dynamic Libaray

---
+++ 0x18-dynamic_libraries/_memset.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * _memset - fills memory with a constant byte.
+ *
+ * @s: pointer to char.
+ * @b: constant byte.
+ * @n: number of bytes.
+ *
+ * Return: pointer to char s.
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i;
+
+	i = 0;
+
+	while (i < n)
+	{
+		s[i] = b;
+		i++;
+	}
+	return (s);
+}
---
+++ 0x18-dynamic_libraries/_strncpy.c
@@ -0,0 +1,32 @@
+#include "main.h"
+/**
+ * _strncpy -  work exactly like strncpy.
+ *
+ * @dest: Input string.
+ *
+ * @src: Input string.
+ *
+ * @n: Input Integer.
+ *
+ * Return: A pointer to the resulting string dest.
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int count1, count2, i;
+
+	count1 = 0;
+	count2 = 0;
+
+	while (dest[count1] != '\0')
+		count1++;
+	while (src[count2] != '\0')
+		count2++;
+	for (i = 0; i < n; i++)
+	{
+		dest[i] = src[i];
+		if (i > count2)
+			dest[i] = '\0';
+	}
+	return (dest);
+}
