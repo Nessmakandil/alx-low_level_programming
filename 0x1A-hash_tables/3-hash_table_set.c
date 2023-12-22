0180075d35c17b1c23a40007bd3a56c7d4215006
NessmaKandil <nessmakandil137@gmail.com>
2023-12-23 00:06:40 +0200

    insert item

---
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -0,0 +1,24 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_set - create item in the table
+ * @ht: created table
+ * @key: string used to create the index
+ * @value: value to be stored
+ * Return: item set correctly (1) or not (0)
+ */
+
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+	unsigned long int index = key_index((const unsigned char *)key, ht->size);
+	hash_node_t *new_item = (hash_node_t *) malloc(sizeof(hash_node_t));
+
+	new_item->key = (char *) key;
+	new_item->value = (char *) value;
+	ht->array[index] = new_item;
+	if (ht->array[index])
+	{
+		return (1);
+	}
+	return (0);
+}
