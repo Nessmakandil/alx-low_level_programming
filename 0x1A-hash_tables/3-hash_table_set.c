487295275ab068e76f775cb605d48341f8ade0bd
NessmaKandil <nessmakandil137@gmail.com>
2023-12-23 03:10:20 +0200

    insert item

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -10,21 +10,40 @@
 
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-	unsigned long int index = key_index((const unsigned char *)key, ht->size);
-	hash_node_t *new_item = (hash_node_t *) malloc(sizeof(hash_node_t));
+	unsigned long int index;
+	hash_node_t *new_node, *tmp;
 
-	new_item->key = (char *) key;
-	new_item->value = (char *) value;
-	if (ht->array[index] == NULL)
+	if (ht == NULL || key == NULL || *key == '\0')
+		return (0);
+
+	index = key_index((const unsigned char *)key, ht->size);
+	tmp = ht->array[index];
+
+	while (tmp != NULL)
 	{
-		ht->array[index] = new_item;
-		return (1);
+		if (strcmp(tmp->key, key) == 0)
+		{
+			new_node = malloc(sizeof(hash_node_t));
+			if (new_node == NULL)
+				return (0);
+
+			new_node->key = (char *) key;
+			new_node->value = (char *) value;
+			new_node->next = tmp->next;
+			tmp->next = new_node;
+			return (1);
+		}
+		tmp = tmp->next;
 	}
-	else if(ht->array[index] != NULL)
-	{
-		new_item->next = *(ht->array);
-		*(ht->array) = new_item;
-		return (1);
-	}
-	return (0);
+
+	new_node = (hash_node_t *) malloc(sizeof(hash_node_t));
+	if (new_node == NULL)
+		return (0);
+
+	new_node->key = (char *) key;
+	new_node->value = (char *) value;
+	new_node->next = ht->array[index];
+	ht->array[index] = new_node;
+
+	return (1);
 }
--- 0x1A-hash_tables/3-main.c
+++ 0x1A-hash_tables/3-main.c
@@ -8,11 +8,34 @@
  *
  * Return: Always EXIT_SUCCESS.
  */
+
+void print_table(hash_table_t *table)
+{
+    unsigned long int i;
+
+    printf("\nHash Table\n-------------------\n");
+
+    for (i = 0; i < table -> size; i++)
+    {
+        if (table -> array[i])
+        {
+            printf("Index:%lu, Key:%s, Value:%s\n", i, table -> array[i] -> key, table -> array[i] -> value);
+        }
+    }
+
+    printf("-------------------\n\n");
+}
+
 int main(void)
 {
     hash_table_t *ht;
 
     ht = hash_table_create(1024);
     hash_table_set(ht, "betty", "cool");
+    hash_table_set(ht, "hetairas", "cool");
+    hash_table_set(ht, "mentioner", "cool");
+    hash_table_set(ht, "heliotropes", "cool");
+    hash_table_set(ht, "neurospora", "cool");
+    print_table(ht);
     return (EXIT_SUCCESS);
 }
