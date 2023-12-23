78e6c51fefb4697348689cab7d688c05ace38b62
NessmaKandil <nessmakandil137@gmail.com>
2023-12-23 22:12:01 +0200

    get item

---
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -0,0 +1,16 @@
+#include "hash_tables.h"
+
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+    hash_node_t *temp;
+    unsigned long int idx;
+
+    idx = key_index((unsigned char *) key, ht->size);
+    temp = ht->array[idx];
+    if (temp == NULL)
+    {
+        return (NULL);
+    }
+    
+    return (temp->value);
+}
